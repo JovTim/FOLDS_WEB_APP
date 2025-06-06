46847cecc8ea3806f50c1a674d978e0fd5eb9881
JovTim <202280218@psu.palawan.edu.ph>
2025-06-06 12:43:19 +0800

    [adjut] converted index.html to php to link nav bar [feat] Can now fetch data from database

---
+++ public/includes/nav.php
@@ -0,0 +1,20 @@
+<nav class="text-black px-4 mx-10 pt-4">
+  <div class="max-w-7xl mx-auto flex justify-between items-center">
+    <!-- Navigation Links -->
+    <div class="flex justify-between w-full">
+      <div class="flex justify-start">
+        <div class="hidden md:flex">
+          <a
+            href="#"
+            class="px-3 pt-2 border-t-2 border-l-2 border-r-2 rounded-t-lg border-black hover:font-bold">FOLDERS</a>
+          <a
+            href="#"
+            class="px-3 pt-2 border-t-2 border-l-2 border-r-2 rounded-t-lg border-black bg-black text-white hover:font-bold">ARCHIVES</a>
+        </div>
+      </div>
+      <a
+        href="#"
+        class="px-3 pt-2 border-t-2 border-l-2 border-r-2 rounded-t-lg border-black bg-black text-white hover:font-bold">ABOUT</a>
+    </div>
+  </div>
+</nav>
