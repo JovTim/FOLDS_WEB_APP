a260292c08420a00103402caf8f1fe90d6161652
JovTim <202280218@psu.palawan.edu.ph>
2025-06-14 17:51:16 +0800

    [feat] added logo [feat] added about page

---
+++ public/about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>FOLDS - ABOUT</title>
+  <link rel="icon" type="image/x-icon" href="assets/logo.svg" />
+  <link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+
+<body>
+  <div class="container mx-auto p-4 pt-10 px-10 hidden md:block">
+    <?php include "includes/nav.php"; ?>
+    <div class="p-10 mx-10 border-2 border-black">
+      <div class="flex justify-center">
+        <img
+          class="w-64 h-auto"
+          src="assets/about-logo.png"
+          alt="FOLDS Logo" />
+      </div>
+      <h1 class="flex justify-center pt-10 text-6xl font-extrabold">FOLDS</h1>
+      <div class="flex justify-center px-7">
+        <p class="pt-5 text-lg text-center">
+          <span class="font-extrabold">FOLDS</span> is a student folder
+          management system developed specifically for College-Based
+          Organizations at Palawan State University (PalSU). It is designed to
+          monitor student folders, particularly during enrollment periods.
+          Typically, students borrow their folders from their respective
+          organization offices to complete their enrollment process. These
+          folders are then processed in the encoding room and may be returned,
+          left unreturned, or occasionally lost. This system is a simplified
+          web-based version of the original
+          <a
+            href="https://github.com/JovTim/STUDENT-FOLDER-MANAGEMENT-APPLICATION"
+            target="_blank"
+            class="font-extrabold hover:text-blue-400">FOLDS System</a>, offering a more minimal approach while retaining its core
+          functionality (TO KEEP TRACK OF FOLDERS).
+        </p>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
